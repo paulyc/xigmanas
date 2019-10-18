864f957c43c1b31c9fbe23c5fd95b9b559d127bf
ms49434 <ms49434@beaaabd7-781f-4c1e-bc4d-5cfe25df7511>
2019-10-18 20:14:02 +0000

    rework

--- branches/11.3.0.4/www/system_sysctl_info.php
+++ branches/11.3.0.4/www/system_sysctl_info.php
@@ -33,29 +33,22 @@
 */
 require_once 'auth.inc';
 require_once 'guiconfig.inc';
-require_once 'co_sphere.php';
-require_once 'properties_sysctl.php';
-require_once 'co_request_method.php';
+require_once 'autoload.php';
 
-function sysctl_info_sphere() {
-	$sphere = new co_sphere_grid('system_sysctl_info','php');
-	$sphere->set_row_identifier('name');
-	$sphere->set_enadis(true);
-	$sphere->set_lock(true);
-	$sphere->grid = [];
-	return $sphere;
-}
-$cop = new systcl_info_properties();
-$sphere = &sysctl_info_sphere();
+use system\sysctl\info\grid_toolbox as toolbox;
+
+//	init properties and sphere
+$cop = toolbox::init_properties();
+$sphere = toolbox::init_sphere();
 unset($output);
-mwexec2('/sbin/sysctl -adeot',$output);
+mwexec2('/sbin/sysctl -adet',$output);
 foreach($output as $row):
 	list($sysctl_name,$sysctl_type,$sysctl_info) = explode('=',$row,3);
-	if(preg_match('/\S/',$sysctl_type)):
+	if(!empty($sysctl_type)):
 		$sphere->grid[$sysctl_name] = ['sysctltype' => $sysctl_type,'sysctlinfo' => $sysctl_info];
 	endif;
 endforeach;
-unset($output);
+unset($row,$output,$sysctl_name,$sysctl_type,$sysctl_info);
 mwexec2('/sbin/sysctl -ae',$output);
 foreach($output as $row):
 	list($sysctl_name,$sysctl_value) = explode('=',$row,2);
@@ -63,69 +56,5 @@ foreach($output as $row):
 		$sphere->grid[$sysctl_name]['value'] = $sysctl_value;
 	endif;
 endforeach;
-unset($output);
-$pgtitle = [gettext('System'),gettext('Advanced'),gettext('sysctl.conf'),gettext('Info')];
-$record_exists = count($sphere->grid) > 0;
-$a_col_width = ['30%','15%','30%','25%'];
-$n_col_width = count($a_col_width);
-//	prepare additional javascript code
-$jcode = $sphere->doj(false);
-if($record_exists):
-	$document = new_page($pgtitle,null,'tablesort');
-else:
-	$document = new_page($pgtitle);
-endif;
-//	get areas
-$body = $document->getElementById('main');
-$pagecontent = $document->getElementById('pagecontent');
-//	add tab navigation
-$document->
-	add_area_tabnav()->
-		push()->
-		add_tabnav_upper()->
-			ins_tabnav_record('system_advanced.php',gettext('Advanced'))->
-			ins_tabnav_record('system_email.php',gettext('Email'))->
-			ins_tabnav_record('system_email_reports.php',gettext('Email Reports'))->
-			ins_tabnav_record('system_monitoring.php',gettext('Monitoring'))->
-			ins_tabnav_record('system_swap.php',gettext('Swap'))->
-			ins_tabnav_record('system_rc.php',gettext('Command Scripts'))->
-			ins_tabnav_record('system_cron.php',gettext('Cron'))->
-			ins_tabnav_record('system_loaderconf.php',gettext('loader.conf'))->
-			ins_tabnav_record('system_rcconf.php',gettext('rc.conf'))->
-			ins_tabnav_record('system_sysctl.php',gettext('sysctl.conf'),gettext('Reload page'),true)->
-			ins_tabnav_record('system_syslogconf.php',gettext('syslog.conf'))->
-		pop()->
-		add_tabnav_lower()->
-			ins_tabnav_record('system_sysctl.php',gettext('sysctl.conf'))->
-			ins_tabnav_record('system_sysctl_info.php',gettext('Information'),gettext('Reload page'),true);
-//	create data area
-$content = $pagecontent->add_area_data();
-//	display information, warnings and errors
-if(file_exists($d_sysrebootreqd_path)):
-	$content->ins_info_box(get_std_save_message(0));
-endif;
-$table = $content->add_table_data_selection();
-$table->ins_colgroup_with_styles('width',$a_col_width);
-$thead = $table->addTHEAD();
-$tbody = $table->addTBODY();
-$tfoot = $table->addTFOOT();
-$thead->
-	ins_titleline(gettext('Overview'),$n_col_width)->
-	addTR()->
-		insTHwC('lhell',$cop->get_name()->get_title())->
-		insTHwC('lhell',$cop->get_sysctltype()->get_title())->
-		insTHwC('lhell',$cop->get_sysctlinfo()->get_title())->
-		insTHwC('lhebl',$cop->get_value()->get_title());
-if($record_exists):
-	foreach($sphere->grid as $sphere->row_id => $sphere->row):
-		$tbody->
-			addTR()->
-				insTDwC('lcell',$sphere->row_id ?? '')->
-				insTDwC('lcell',$sphere->row[$cop->get_sysctltype()->get_name()] ?? '')->
-				insTDwC('lcell',$sphere->row[$cop->get_sysctlinfo()->get_name()] ?? '')->
-				insTDwC('lcebl',$sphere->row[$cop->get_value()->get_name()] ?? '');
-	endforeach;
-else:
-	$tbody->ins_no_records_found($n_col_width);
-endif;
-$document->render();
+unset($row,$output,$sysctl_name,$sysctl_value);
+toolbox::render($cop,$sphere);
